80d81ef5f7b66c23a29a5d92b07e2f4928dcd0f5
ishanrana363 <ishanrana363@gmail.com>
2025-06-04 19:54:30 +0600

    first commit

---
+++ alter.sql
@@ -0,0 +1,24 @@
+-- Active: 1749042118135@@127.0.0.1@5432@phadmin
+-- SELECT * FROM person;
+
+-- ALTER TABLE person
+--     ADD COLUMN email VARCHAR(50) DEFAULT 'default@gmail.com' NOT NULL;
+
+-- SELECT * FROM person;
+
+-- ALTER TABLE person
+--     DROP COLUMN email;
+
+-- SELECT * FROM person;
+
+-- ALTER TABLE person
+--     ADD COLUMN email VARCHAR(50) DEFAULT 'ishanrana094@gmail.com' NOT NULL;
+
+-- SELECT * FROM person;
+
+-- ALTER TABLE person
+--     RENAME COLUMN email to email_name;
+
+-- SELECT * FROM person;
+
+
